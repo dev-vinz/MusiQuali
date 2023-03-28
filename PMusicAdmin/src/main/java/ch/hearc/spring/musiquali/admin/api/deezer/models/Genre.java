e7a63f49d1621eb5f2ef77b146480dd5711eab8f
!+VinZ+! <jeannin.vincent@gmail.com>
2023-03-28 09:42:33 +0200

    feat(#4) : Add genre model

--- PMusicAdmin/src/main/java/ch/hearc/spring/musiquali/admin/api/deezer/models/Genre.java
+++ PMusicAdmin/src/main/java/ch/hearc/spring/musiquali/admin/api/deezer/models/Genre.java
@@ -18,6 +18,60 @@ public class Genre
 	|*				Get				*|
 	\*------------------------------*/
 
+	/**
+	 * Gets the ID
+	 * @return An ID
+	 */
+	public Long getId()
+		{
+		return this.id;
+		}
+
+	/**
+	 * Gets the name
+	 * @return A name
+	 */
+	public String getName()
+		{
+		return this.name;
+		}
+
+	/**
+	 * Gets the picture in small size
+	 * @return A small picture
+	 */
+	public String getPictureSmall()
+		{
+		return this.pictureSmall;
+		}
+
+	/**
+	 * Gets the picture in medium size
+	 * @return A medium picture
+	 */
+	public String getPictureMedium()
+		{
+		return this.pictureMedium;
+		}
+
+	/**
+	 * Gets the picture in big size
+	 * @return A big picture
+	 */
+	public String getPictureBig()
+		{
+		return this.pictureBig;
+		}
+
+	/**
+	 * Gets the picture in XL size
+	 * @return A XL picture
+	 */
+	public String getPictureXL()
+		{
+		return this.pictureXL;
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
@@ -31,4 +85,16 @@ public class Genre
 
 	@JsonProperty("name")
 	private String name;
+
+	@JsonProperty("picture_small")
+	private String pictureSmall;
+
+	@JsonProperty("picture_medium")
+	private String pictureMedium;
+
+	@JsonProperty("picture_big")
+	private String pictureBig;
+
+	@JsonProperty("picture_XL")
+	private String pictureXL;
 	}
